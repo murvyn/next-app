dff3f12775c590e9b1f2e23c43a3b8973a7b9517
Marvin Asamoah <marvin.asamoah.123@gmail.com>
2023-10-31 00:38:53 +0000

    remove code

--- app/api/send-email/route.tsx
+++ app/api/send-email/route.tsx
@@ -1,15 +1,15 @@
-import { Resend } from "resend";
-import WelcomeTemplate from "@/emails/WelcomeTemplate";
-import { NextResponse } from "next/server";
+// import { Resend } from "resend";
+// import WelcomeTemplate from "@/emails/WelcomeTemplate";
+// import { NextResponse } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY)
+// const resend = new Resend(process.env.RESEND_API_KEY)
 
-export async function POST(){
-   await resend.emails.send({
-        from: '...',
-        to: 'programing.com',
-        subject: '...',
-        react: <WelcomeTemplate name='mosh' />
-  })
-  return NextResponse.json({})
-}
+// export async function POST(){
+//    await resend.emails.send({
+//         from: '...',
+//         to: 'programing.com',
+//         subject: '...',
+//         react: <WelcomeTemplate name='mosh' />
+//   })
+//   return NextResponse.json({})
+// }

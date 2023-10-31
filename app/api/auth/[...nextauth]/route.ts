11d6051776ba9d82d4132c0c56cb6a25745db324
Marvin Asamoah <marvin.asamoah.123@gmail.com>
2023-10-31 00:16:14 +0000

    fix errors

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,47 +1,8 @@
-import { prisma } from "@/prisma/client";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { NextAuthOptions } from "next-auth";
 import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcrypt";
+import { authOptions } from "../authOptions";
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "email", placeholder: "Email" },
-        password: {
-          label: "Password",
-          type: "password",
-          placeholder: "Password",
-        },
-      },
-      async authorize(credentials, req) {
-        if (!credentials?.email || !credentials?.password) return null;
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
 
-        if (!user) return null;
-        const passwordMatch = await bcrypt.compare(
-          credentials.password,
-          user.hashedPassword!
-        );
-        return passwordMatch ? user : null
-      },
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  adapter: PrismaAdapter(prisma),
-  session: {
-    strategy: "jwt",
-  },
-};
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,46 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import { useState } from "react";
+import dynamic from "next/dynamic";
+// import _ from 'lodash'
 
-export default async function Home() {
-  const session = await getServerSession(authOptions)
+// const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+//   ssr: false,
+//   loading: () => <p>loading</p>,
+// });
+
+export default function Home() {
+  // const session = await getServerSession(authOptions);
+  // const [isVisible, setIsVisible] = useState(false);
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+    <main className="relative h-screen">
+      {/* <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href='/users'>users</Link>
-      <ProductCard />
+      <ProductCard /> */}
+      {/* <Image
+        src="https://bit.ly/react-cover"
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        fill
+        alt="coffee"
+        quality={100}
+        priority
+      /> */}
+      <button onClick={async () => {
+        const _ = (await import('lodash')).default
+        const users = [
+          {name: 'c'},
+          {name: 'b'},
+          {name: 'a'},
+        ]
+       const sorted =  _.sortBy(users, ['name'])
+       console.log(sorted)
+      }}>Show</button>
+      {/* {isVisible && <HeavyComponent />} */}
     </main>
   );
 }
